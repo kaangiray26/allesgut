f5d3f5751c57dc778b52117413a75a759f81e204
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2022-10-28 11:05:04 +0200

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/zsh
+
+# abort on errors
+set -e
+
+# remove old dist
+rm -rf src/dist
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd src/dist
+
+git init
+
+git add .
+
+git commit -m "deploy"
+
+git remote add origin git@github.com:kaangiray26/allesgut.git
+
+git push --force origin master:gh-pages
+
+cd ../..
